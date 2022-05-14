514b1d5c426ec4dbd7c46cd1ed750bf220d05bb6
Marzio95 <marziodellarocca14@gmail.com>
2022-05-14 12:18:25 +0200

    inserisco le validation anche nell edit di modifica di un fumetto e al  controller

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -14,6 +14,9 @@
             <div class="col-sm-10">
                 <input value="{{ $comic->title }}" type="text" class="form-control" id="title" placeholder="Title"
                     name="title">
+                @error('title')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
         </div>
         <div class="form-group row">
@@ -21,6 +24,9 @@
             <div class="col-sm-10">
                 <input value="{{ $comic->description }}" type="text" class="form-control" id="description"
                     placeholder="Description" name="description">
+                @error('description')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
         </div>
         <div class="form-group row">
@@ -28,6 +34,9 @@
             <div class="col-sm-10">
                 <input value="{{ $comic->thumb }}" type="text" class="form-control" id="thumb" placeholder="Foto Comic"
                     name="thumb">
+                @error('thumb')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
         </div>
         <div class="form-group row">
@@ -35,6 +44,9 @@
             <div class="col-sm-10">
                 <input value="{{ $comic->price }}" type="number" class="form-control" id="price" placeholder="Prezzo"
                     name="price">
+                @error('price')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
         </div>
         <div class="form-group row">
@@ -42,17 +54,26 @@
             <div class="col-sm-10">
                 <input value="{{ $comic->series }}" type="text" class="form-control" id="series"
                     placeholder="Serie Fumetto" name="series">
+                @error('series')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
         </div>
         <div class="form-group row mt-3 mb-3">
             <label for="sale_date">Data di uscita</label>
             <input value="{{ $comic->sale_date }}" type="date" id="sale_date" name="sale_date">
+            @error('sale_date')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group row">
             <label for="type" class="col-sm-2 col-form-label">Genere Fumetto</label>
             <div class="col-sm-10">
                 <input value="{{ $comic->type }}" type="text" class="form-control" id="type"
                     placeholder="Genere Fumetto" name="type">
+                @error('type')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
         </div>
         <div class="form-group row mt-5 d-flex">
@@ -62,6 +83,18 @@
             <div class="col-sm-10 mt-5">
                 <a class="text-white bg-black p-2" href="{{ url()->previous() }}">Torna indietro</a>
             </div>
+            <div class="col-sm-10 mt-5">
+                <a class="text-white bg-black p-2" href="{{ route('index', 'ComicController') }}">Torna alla lista</a>
+            </div>
         </div>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </form>
 @endsection
